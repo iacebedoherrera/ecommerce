9c396ea2fa3a2aaf1ee8801b08b7d08342d885c1
ivannano <iacebedoherrera@gmail.com>
2024-04-25 18:34:42 +0200

    [ECOM-1] Se añade clase User y Address

--- ecommerce/dal/models/user.py
+++ ecommerce/dal/models/user.py
@@ -1,13 +1,34 @@
 import reflex as rx
-from sqlmodel import Field
-from typing import Optional
+from sqlmodel import Field, Relationship
+from typing import Optional, List
+
+
+class Address(rx.Model, table=True):
+    id: int = Field(default=None, primary_key=True)
+    address: str
+    city: str
+    autonomous_community: str
+    postal_code: str
+    users: List["User"] = Relationship(back_populates="address")
+
+    
+    def __str__(self):
+        return (
+            f"{self.address}, \n"
+            f"{self.city}, \n"
+            f"{self.autonomous_community}, \n"
+            f"{self.postal_code}"
+        )
+    
 
 class User(rx.Model, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name: str
     surname: str
     email: str = Field(unique=True)
     password: str
-    address_id: Optional[int] = Field(default=None)
     phone_number: str
     disabled: bool = Field(default=False)
+    address_id: Optional[int] = Field(default=None, foreign_key="address.id")
+    address: Optional[Address] = Relationship(back_populates="users")
+
