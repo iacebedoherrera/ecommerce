b487e6f2f2154b989803fd01e9ab6761ee0066ff
ivannano <iacebedoherrera@gmail.com>
2024-04-25 18:33:29 +0200

    [ECOM-6] Pantalla Productos

--- ecommerce/const.py
+++ ecommerce/const.py
@@ -3,4 +3,12 @@ SHOP_NAME = "I&N Shop"
 
 # COOKIES
 ALGORITHM = "HS256"
-ACCESS_TOKEN_DURATION = 30
+ACCESS_TOKEN_DURATION = 3600
+LOG_IN_COOKIE_NAME = "jwt"
+USERNAME_COOKIE_NAME = "name"
+
+# PRODUCTOS
+PRODUCTS: dict = {
+    "tshirt":"Camisetas",
+    "sweatshirt":"Sudaderas"
+}
